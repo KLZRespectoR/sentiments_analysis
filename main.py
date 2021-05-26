6945b02e89900d8bbe5e0e9222cb8e0cd59ff896
KLZRespectoR <alhmeidi@hotmail.com>
2021-05-26 12:17:06 +0200

    trumph_analysis

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # 1. Create a text file and take text from it (read the text)
 import nltk as nltk
 
-text= open('obama.txt',encoding='utf-8').read()
+text= open('trumph.txt',encoding='utf-8').read()
 #print(text)
 
 # 2. convert the letter into lowercase
@@ -15,35 +15,13 @@ import string
 
 cleaned_text= lower_case.translate(str.maketrans('','',string.punctuation))
 #print(cleaned_text)
-
-#Tokenization and stop words:
-#tokenized_words= cleaned_text.split()
-# or you can use the word_tokenizer by nltk
-#print(tokenized_words)
-#print(len(cleaned_text))
-#print(len(tokenized_words))
-
-
-
-#import nltk
-#nltk.download()
-
+print(len(cleaned_text))
 
 from nltk.corpus import wordnet
 from nltk.corpus import stopwords
 sw= stopwords.words('english')
 #print(sw)
 
-#final_words=[]
-#or word in tokenized_words:
- #   if word not in sw:
-   #     final_words.append(word)
-#print(final_words)
-#print(len(final_words))
-
-
-
-#5. Algorithem for emotion and text analysis: a list of words with their emotions attitude.
 #6. using nltk tokenizer
 from nltk.tokenize import word_tokenize
 tokenized_words= word_tokenize(cleaned_text, "english")
@@ -54,21 +32,31 @@ final_words=[]
 for word in tokenized_words:
     if word not in sw:
         final_words.append(word)
-#print(final_words)
-#print(len(final_words))
-
+print(final_words)
+print(len(final_words))
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
 def sentiment_analyse(sentiment_text):
-    score= SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
-    neg= score['neg']
-    pos= score['pos']
-    if neg> pos:
-        print("Negative Sentiment")
-    elif pos> neg:
-        print("Positive Sentiment")
-    else:
-        print("Neutral Vibes")
-        
-    #print(score)
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    print(score)
+    neg = score['neg']
+    pos = score['pos']
+    #if neg > pos:
+        #print("Negative Sentiment")
+    #elif pos > neg:
+        #print("Positive Sentiment")
+    #else:
+        #print("Neutral Vibes")
+
+
+
+
 sentiment_analyse(cleaned_text)
+
+
+
+
+
+
